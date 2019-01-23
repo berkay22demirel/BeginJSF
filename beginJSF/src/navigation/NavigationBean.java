ce595bd55a8d674fe3afb58708a0e29853f7c26b
berkay22demirel <berkay_demirel22@hotmail.com>
2019-01-23 09:50:32 +0300

    static navigation example

---
+++ beginJSF/src/navigation/NavigationBean.java
@@ -0,0 +1,14 @@
+package navigation;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class NavigationBean {
+
+	public String goToPage2() {
+		return "page2";
+	}
+
+}

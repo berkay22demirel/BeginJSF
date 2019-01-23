42de0000250395953eb730c7f7229eff0fca1c6d
berkay22demirel <berkay_demirel22@hotmail.com>
2019-01-23 09:27:08 +0300

    expression language example

---
+++ beginJSF/src/expressionlanguage/ExpressionLanguageExample.java
@@ -0,0 +1,68 @@
+package expressionlanguage;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class ExpressionLanguageExample {
+
+	private String key;
+	private String value;
+	private String selectedFramework;
+	private Map<String, Object> frameworkMap;
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public String getSelectedFramework() {
+		return selectedFramework;
+	}
+
+	public void setSelectedFramework(String selectedFramework) {
+		this.selectedFramework = selectedFramework;
+	}
+
+	public Map<String, Object> getFrameworkMap() {
+		return frameworkMap;
+	}
+
+	public void setFrameworkMap(Map<String, Object> frameworkMap) {
+		this.frameworkMap = frameworkMap;
+	}
+
+	@PostConstruct
+	public void init() {
+
+		frameworkMap = new LinkedHashMap<String, Object>();
+		frameworkMap.put("Servlet", "Servlet");
+		frameworkMap.put("JSP", "JSP");
+		frameworkMap.put("Hibernate", "Hibernate");
+
+	}
+
+	public void addFramework(String key, String value) {
+		if (key != "" && value != "") {
+			frameworkMap.put(key, value);
+		}
+	}
+
+
+}
